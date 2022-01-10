78a35b3cb7c6a983d4b2bf91ea62b25fb153408e
Krystian Spiewak <krystianspiewak@gmail.com>
2022-01-10 14:30:47 -0500

    Using index buffer and defining constexpr

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,3 +1,5 @@
+#include "Application.h"
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -6,7 +8,7 @@
 #include <sstream>
 #include <string>
 
-static std::string LoadShaderFile(const std::string& filepath)
+static std::string LoadShaderFile(const char* filepath)
 {
 	const std::ifstream stream(filepath);
 	std::stringstream buffer;
@@ -83,19 +85,36 @@ int main(void) {
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	constexpr float positions[6] = {
+	constexpr float positions[] = {
 		-0.5f, -0.5f,
-		 0.0f,  0.5f,
-		 0.5f, -0.5f
+		 0.5f, -0.5f, 
+		 0.5f,  0.5f,
+		-0.5f,  0.5f
 	};
+
+	const uint32_t indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	constexpr auto triangle_size = TRIANGLE_VERTS * VERT_SIZE;
+	constexpr auto triangles_count = std::size(indices) / TRIANGLE_VERTS;
+	constexpr auto buffer_size = triangle_size * triangles_count * sizeof(positions[0]);
+	constexpr auto index_buffer_size = buffer_size / triangles_count;
 
 	uint32_t buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, buffer_size, positions, GL_STATIC_DRAW);
 
+	constexpr auto vertex_offset = VERT_SIZE * sizeof(positions[0]);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, vertex_offset, nullptr);
+
+	uint32_t index_buffer;
+	glGenBuffers(1, &index_buffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_buffer_size, indices, GL_STATIC_DRAW);
 
 	const std::string vertexShader = LoadShaderFile("res/shaders/Basic.vert");
 	const std::string fragmentShader = LoadShaderFile("res/shaders/Basic.frag");
@@ -108,7 +127,7 @@ int main(void) {
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, triangle_size, GL_UNSIGNED_INT, nullptr);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
---
+++ OpenGL/src/Application.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/**
+ * \brief Defines the number of vertices per triangle
+ */
+#define TRIANGLE_VERTS 3
+/**
+ * \brief Defines the size of each vertex
+ */
+#define VERT_SIZE 2
