84ff02f824897eea242e5632e13f94d33dcf0d4a
Krystian Spiewak <krystianspiewak@gmail.com>
2022-01-09 22:26:40 -0500

    VertexAttribArray enabled, triangle drawn, no shader yet

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -31,10 +31,13 @@ int main(void) {
 		 0.5f, -0.5f
 	};
 
-	unsigned int buffer;
+	uint32_t buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
