69a442c4846309685ed205fde8d38fa6f161becc
Krystian Spiewak <krystianspiewak@gmail.com>
2022-01-14 11:13:21 -0500

    2D Texture Test setup

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -4,6 +4,7 @@
 #include "imgui_impl_glfw_gl3.h"
 
 #include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
 
 int main(void) {
 	if (!glfwInit())
@@ -37,6 +38,7 @@ int main(void) {
 	currentTest = testMenu;
 
 	testMenu->RegisterTest<test::TestClearColor>("Clear Color");
+	testMenu->RegisterTest<test::TestTexture2D>("2D Texture");
 
 	while (!glfwWindowShouldClose(window))
 	{
